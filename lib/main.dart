58ce69d4986f2ed7463ded802005271cb54789de
Jonathan Peñaloza <thekill8593@hotmail.com>
2018-12-03 11:55:40 -0500

    add reveal effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:page_reveal/page_reveal.dart';
+import 'package:page_reveal/pages.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,6 +24,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Hello World")));
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        Page(
+          viewModel: pages[0],
+          percentVisible: 1.0,
+        ),
+        PageReveal(
+          revealPercent: 1.0,
+          child: Page(
+            viewModel: pages[1],
+            percentVisible: 1.0,
+          ),
+        )
+      ],
+    ));
   }
 }
